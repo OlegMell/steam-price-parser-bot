5ee12f9c325f7ff3b55d2dcff263d8e1a4b7449a
Oleg Melnik <oleg.mel123@gmail.com>
2022-08-11 22:13:49 +0300

    dev

--- src/bot/start.ts
+++ src/bot/start.ts
@@ -19,29 +19,29 @@ export default async function startParseBot() {
 
     bot = new Telegraf(process.env.BOT_TOKEN || '')
 
-    const app = express();
+    // const app = express();
 
-    const port = process.env.PORT || 5000;
+    // const port = process.env.PORT || 5000;
+    //
+    // app.use(express.json());
 
-    app.use(express.json());
+    // const secretPath = `telegraf/${ bot.secretPathComponent() }`;
 
-    const secretPath = `telegraf/${bot.secretPathComponent()}`;
-
-    app.get('/', (req: Request, res: any) => {
-        console.log(req);
-        res.status(200).json({ message: 'Hello from the Bot API.' });
-    });
+    // app.get('/', (req: Request, res: any) => {
+    //     console.log(req);
+    //     res.status(200).json({ message: 'Hello from the Bot API.' });
+    // });
 
     if (process.env.NODE_ENV === 'production') {
         console.log('here');
-        await bot.telegram.setWebhook(process.env.HEROKU_URL! + secretPath);
+        // await bot.telegram.setWebhook(process.env.HEROKU_URL! + secretPath);
 
-        app.use(bot.webhookCallback(secretPath));
+        // app.use(bot.webhookCallback(secretPath));
     }
 
-    app.listen(port, () => {
-        console.log(`\n\nServer running on port ${ port }.\n\n`);
-    });
+    // app.listen(port, () => {
+    //     console.log(`\n\nServer running on port ${ port }.\n\n`);
+    // });
 
     const stage = setStageScenes();
 
@@ -70,8 +70,14 @@ export default async function startParseBot() {
 
     startMainKeyboardListener(bot);
 
-    if (process.env.NODE_ENV !== 'production')
-        await bot.launch();
+    // bot.startWebhook
+    bot.launch({
+        webhook: {
+            domain: process.env.HEROKU_URL,
+            port: 4000
+        }
+    });
+    // if (process.env.NODE_ENV !== 'production')
 
     // Enable graceful stop
     process.once('SIGINT', () => bot.stop('SIGINT'));
