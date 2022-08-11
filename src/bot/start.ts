041ce1a5569f73a4ecc767d296a92e94496bd8ee
Oleg Melnik <oleg.mel123@gmail.com>
2022-08-11 15:42:01 +0300

    dev

--- src/bot/start.ts
+++ src/bot/start.ts
@@ -9,12 +9,17 @@ import { startMainKeyboardListener } from './mainKeyboardListener.js';
 
 const { Stage } = Scenes;
 
+import express from 'express';
+
+
 
 export default async function startParseBot() {
 
+
+
     await startDBConnect();
 
-    let bot;
+    let bot: any;
 
     if (process.env.NODE_ENV === 'production') {
         bot = new Telegraf(process.env.BOT_TOKEN || '')
@@ -22,6 +27,26 @@ export default async function startParseBot() {
     } else {
         bot = new Telegraf(process.env.BOT_TOKEN || '');
     }
+
+    const app = express();
+
+    const port = process.env.PORT || 5000;
+
+    app.use(express.json());
+
+    app.get('/', (req: Request, res: any) => {
+        console.log(req);
+        res.status(200).json({ message: 'Hello from the Bot API.' });
+    });
+// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
+    app.post(`/${process.env.BOT_TOKEN}`, (req: Request, res: any) => {
+        bot.processUpdate(req.body);
+        res.status(200).json({ message: 'ok' });
+    });
+
+    app.listen(port, () => {
+        console.log(`\n\nServer running on port ${port}.\n\n`);
+    });
 
     const sceneGenerator = new SceneGenerator();
 
