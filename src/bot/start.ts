00d402c4978ce2ea90cf1f3dba4338c74766f26c
Oleg Melnik <oleg.mel123@gmail.com>
2022-08-13 23:13:04 +0300

    dev

--- src/bot/start.ts
+++ src/bot/start.ts
@@ -131,7 +131,8 @@ export default async function startParseBot() {
                         await page.goto(userItem.link, { waitUntil: 'networkidle2' });
 
                         page.waitForSelector('span.market_commodity_orders_header_promote', {
-                            timeout: 80000
+                            timeout: 80000,
+                            visible: true
                         }).then(async () => {
                             const c = await page.content();
                             const dom = new JSDOM(c);
